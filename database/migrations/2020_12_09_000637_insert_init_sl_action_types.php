74f52b1cb360f3907a2082710f1223c7cc63285a
Nawwar Elnarsh <ne@zeitgeist.group>
2023-08-12 18:30:24 +0200

    SLARA-1 | Rename migration class to match Sl prefix

--- database/migrations/2020_12_09_000637_insert_init_sl_action_types.php
+++ database/migrations/2020_12_09_000637_insert_init_sl_action_types.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class InsertInitActionTypes extends Migration
+class InsertSlInitActionTypes extends Migration
 {
     /**
      * Run the migrations.
