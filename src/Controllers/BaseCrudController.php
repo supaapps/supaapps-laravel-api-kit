2ddc9a4f962139909f114f47b92dfd8b85793c0e
Nawwar Elnarsh <ne@zeitgeist.group>
2023-08-12 19:09:11 +0200

    SLARA-1 | Add BaseCrudController

---
+++ src/Controllers/BaseCrudController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Supaapps\Supalara\Controllers;
+
+
+class BaseCrudController extends Controller
+{
+    public bool $shouldPaginate = false;
+    public string $model;
+    public bool $isDeletable = false;
+    public bool $readOnly = false;
+    public ?string $searchField = null;
+}
--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -6,10 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class BaseModel extends Model
 {
-    public bool $isDeletable = true;
-    public bool $readOnly = false;
-    public bool $shouldPaginate = false;
-
     public function reloadRelations()
     {
         return $this->load($this->with);
