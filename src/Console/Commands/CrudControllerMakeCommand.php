cbef14f723710f3ac5b759d82afdfee36c8baea3
Amro Kamh <wshcylinder@gmail.com>
2023-10-04 17:18:34 +0300

    SLARA-14 | Search by column property

--- src/Console/Commands/CrudControllerMakeCommand.php
+++ src/Console/Commands/CrudControllerMakeCommand.php
@@ -3,18 +3,22 @@
 namespace Supaapps\Supalara\Console\Commands;
 
 use Illuminate\Routing\Console\ControllerMakeCommand;
+use Illuminate\Support\Facades\Schema;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 use function Laravel\Prompts\confirm;
+use function Laravel\Prompts\select;
 
 class CrudControllerMakeCommand extends ControllerMakeCommand
 {
     protected $name = 'make:crud-controller';
 
     protected $description = 'Create a new CRUD controller';
+
+    private array $tableColumns = [];
 
     protected function getStub()
     {
@@ -34,6 +38,7 @@ class CrudControllerMakeCommand extends ControllerMakeCommand
             ['shouldPaginate', null, InputOption::VALUE_OPTIONAL, 'Indicates the index should return paginated response', false],
             ['isDeletable', null, InputOption::VALUE_OPTIONAL, 'The model can be deleted', false],
             ['readOnly', null, InputOption::VALUE_OPTIONAL, 'The model is for read only', false],
+            ['searchField', null, InputOption::VALUE_OPTIONAL, 'Search by single column', null],
         ];
     }
 
@@ -97,6 +102,7 @@ class CrudControllerMakeCommand extends ControllerMakeCommand
         $this->replaceOnlyIfOptionChanged('shouldPaginate', 'public bool $shouldPaginate', $replace);
         $this->replaceOnlyIfOptionChanged('isDeletable', 'public bool $isDeletable', $replace);
         $this->replaceOnlyIfOptionChanged('readOnly', 'public bool $readOnly', $replace);
+        $this->replaceOnlyIfOptionChanged('searchField', 'public ?string $searchField', $replace);
     }
 
     private function replaceOnlyIfOptionChanged(string $key, string $stubLine, array &$replace): void
@@ -110,20 +116,45 @@ class CrudControllerMakeCommand extends ControllerMakeCommand
 
     protected function afterPromptingForMissingArguments(InputInterface $input, OutputInterface $output)
     {
-        $input->setOption('shouldPaginate', confirm(
-            label: 'Is index should return shouldPaginate response?',
-            default: $this->option('shouldPaginate')
-        ));
+        $this->setModelColumns();
 
-        $input->setOption('isDeletable', confirm(
-            label: 'Is the model isDeletable?',
-            default: $this->option('isDeletable')
-        ));
+        $input->setOption(
+            'shouldPaginate',
+            confirm('Is index should return paginated response?', $this->option('shouldPaginate'))
+        );
 
-        $input->setOption('readOnly', confirm(
-            label: 'Is the model for read only?',
-            default: $this->option('readOnly')
-        ));
+        $input->setOption(
+            'isDeletable',
+            confirm('Is the model can be deleted?', $this->option('isDeletable'))
+        );
+
+        $input->setOption(
+            'readOnly',
+            confirm('Is the model for read only?', $this->option('readOnly'))
+        );
+
+        if (is_null($this->option('searchField')) && !empty($this->tableColumns)) {
+            $searchByField = confirm("Do yo want to search by single column?", false);
+
+            if ($searchByField) {
+                $input->setOption('searchField', select(
+                    'Enter column name to search with:',
+                    $this->tableColumns,
+                    $this->option('searchField')
+                ));
+            }
+        }
+
+    }
+
+    private function setModelColumns(): void
+    {
+        $modelClass = $this->parseModel($this->argument('model'));
+
+        if (class_exists($modelClass)) {
+            $table = app($modelClass)->getTable();
+            $this->tableColumns = Schema::getColumnListing($table);
+        }
     }
 
     protected function promptForMissingArgumentsUsing()
