790424b53821926425ba55d0f86f8213d3c57f5c
Amro Kamh <wshcylinder@gmail.com>
2023-11-09 09:48:06 +0200

    SLARA-17 | Add test cases for destroy trait

---
+++ tests/Feature/Controllers/CrudTraits/CrudDestroyTraitTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature\Controllers\CrudTraits;
+
+use Tests\TestCase;
+use Tests\Stubs\SupaLaraExampleModel;
+
+class CrudDestroyTraitTest extends TestCase
+{
+    public function testItCanNotDeleteModelWhenNonDeletePropertyIsFalse(): void
+    {
+        $model = SupaLaraExampleModel::factory()->create();
+
+        $response = $this->deleteJson("/non-deletable-example/{$model->id}");
+
+        $response->assertUnauthorized();
+    }
+
+    public function testItCanNotDeleteModelWhenReadOnlyPropertyIsTrue(): void
+    {
+        $model = SupaLaraExampleModel::factory()->create();
+
+        $response = $this->deleteJson("/readonly-example/{$model->id}");
+
+        $response->assertUnauthorized();
+    }
+
+    public function testItCanDeleteTheModel(): void
+    {
+        $models = SupaLaraExampleModel::factory(2)
+            ->create();
+
+        $response = $this->deleteJson("/examples/{$models->first()->id}");
+
+        $response->assertNoContent();
+        $this->assertModelMissing($models->first());
+        $this->assertModelExists($models->get(1));
+    }
+}
